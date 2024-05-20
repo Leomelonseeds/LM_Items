0766faa91193c35598ac224093f1e1acbd217cac
Leomelonseeds <leonardfang15@gmail.com>
2024-05-19 23:25:22 -0700

    Add UltimaAddons support, remove unneeded plugins

--- src/io/github/stumper66/lm_items/LM_Items.java
+++ src/io/github/stumper66/lm_items/LM_Items.java
@@ -1,15 +1,15 @@
 package io.github.stumper66.lm_items;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class LM_Items extends JavaPlugin implements LM_Items_API {
     public LM_Items(){
@@ -41,20 +41,9 @@ public class LM_Items extends JavaPlugin implements LM_Items_API {
         Utils.logger.info("LM_Items: building API classes");
 
         final List<String> names = List.of(
-                "Coins",
-                "MMOItems",
-                "ExecutableItems",
-                "EcoAPI",
-                "CustomItems",
-                "ItemsAdder",
-                "MagicHealth",
-                "CustomCrafting",
-                "ExcellentCrates",
                 "HeadDatabase",
-                "ItemEdit",
-                "Oraxen",
                 "MythicMobs",
-                "CombatWeaponryPlusImpl"
+                "UltimaAddons"
         );
 
         for (final String name : names) {
@@ -76,27 +65,6 @@ public class LM_Items extends JavaPlugin implements LM_Items_API {
             classesAreBuilding = false;
         }
 
-        if (!this.supportedPlugins.containsKey("eco") || !this.supportedPlugins.get("eco").getIsInstalled())
-            return;
-
-        // eco plugin is required for all of the following:
-        final List<String> ecoSupported = List.of(
-                "EcoArmor", "EcoCrates", "EcoItems", "Reforges", "Talismans"
-        );
-
-        for (final String name : ecoSupported) {
-            ItemsAPI api = null;
-            try {
-                final Class<?> clazz = Class.forName("io.github.stumper66.lm_items.plugins.ecosupported." + name);
-                api = (ItemsAPI) clazz.getConstructor().newInstance();
-            } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
-                     InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-            if (api != null)
-                this.supportedPlugins.put(api.getName(), api);
-        }
-
     }
 
     private void registerCommands(){
@@ -112,10 +80,12 @@ public class LM_Items extends JavaPlugin implements LM_Items_API {
         this.supportedPlugins.clear();
     }
 
+    @Override
     public boolean doesSupportPlugin(@NotNull final String pluginName){
         return this.supportedPlugins.containsKey(pluginName);
     }
 
+    @Override
     public @Nullable ItemsAPI getItemAPIForPlugin(final @NotNull String pluginName) {
         return this.supportedPlugins.get(pluginName);
     }
--- src/io/github/stumper66/lm_items/plugins/UltimaAddons.java
+++ src/io/github/stumper66/lm_items/plugins/UltimaAddons.java
@@ -1,34 +1,40 @@
 package io.github.stumper66.lm_items.plugins;
 
-import io.github.stumper66.lm_items.ExternalItemRequest;
-import io.github.stumper66.lm_items.GetItemResult;
-import io.github.stumper66.lm_items.ItemsAPI;
-import org.bukkit.Bukkit;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Collection;
 import java.util.Collections;
 
-@SuppressWarnings("unused")
-public class ItemEdit implements ItemsAPI {
+import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
+
+import io.github.stumper66.lm_items.ExternalItemRequest;
+import io.github.stumper66.lm_items.GetItemResult;
+import io.github.stumper66.lm_items.ItemsAPI;
+
+public class UltimaAddons implements ItemsAPI {
+
+    @Override
     public boolean getIsInstalled() {
         return Bukkit.getPluginManager().getPlugin(getName()) != null;
     }
 
+    @Override
     public @NotNull String getName(){
-        return "ItemEdit";
+        return "UltimaAddons";
     }
 
-    public @NotNull GetItemResult getItem(final @NotNull ExternalItemRequest itemRequest){
+    @Override
+    public @NotNull GetItemResult getItem(@NotNull ExternalItemRequest itemRequest) {
         final GetItemResult result = new GetItemResult(getIsInstalled());
         if (!result.pluginIsInstalled) return result;
-
-        result.itemStack = emanondev.itemedit.ItemEdit.get().getServerStorage().getItem(itemRequest.itemId);
-
+        
+        com.leomelonseeds.ultimaaddons.UltimaAddons ua = com.leomelonseeds.ultimaaddons.UltimaAddons.getPlugin();
+        result.itemStack = ua.getItems().getItem(itemRequest.itemId);
         return result;
     }
 
+    @Override
     public @NotNull Collection<String> getItemTypes() {
         return Collections.emptyList();
     }
+
 }
